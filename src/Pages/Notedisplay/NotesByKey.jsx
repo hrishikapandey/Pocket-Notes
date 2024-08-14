2368a48cdad3d40e9066b81b8828f994ee3d1586
hrishikapandey <hrishiap38@gmail.com>
2024-08-14 20:44:37 +0530

    commited

--- src/Pages/Notedisplay/NotesByKey.jsx
+++ src/Pages/Notedisplay/NotesByKey.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import styles from './NotesByKey.module.css'
+import React from 'react';
+import styles from './NotesByKey.module.css';
 import SideBarOptions from '../../Components/SideBarOptions';
 import Content from '../../Components/Content';
 import Notes from './Notes';
 import useNotes from '../../Hooks/useNotes';
 import { useState, useEffect } from 'react';
 import backBtn from '../../assets/backbtn.png';
+
 const NotesByKey = () => {
-  const { selected, notes, isMobile, setDisplay } = useNotes();
+  const { selected, notes, isMobile, setDisplay, setSelected } = useNotes(); // Assuming setSelected exists
   const newNote = notes.filter((note) => note.id === selected);
   const [notesData, setNotesData] = useState(
     JSON.parse(localStorage.getItem(selected)) || []
@@ -16,43 +17,44 @@ const NotesByKey = () => {
   const handelClick = () => {
     if (isMobile) {
       setDisplay(false);
+    } else {
+     
+      window.location.href = '/homepage';
+      
     }
   };
-
 
   useEffect(() => {
     setNotesData(JSON.parse(localStorage.getItem(selected)) || []);
   }, [selected]);
+
   useEffect(() => {
     console.log("hello");
   }, [notesData]);
+
   return (
     <div className={styles.container}>
       <div className={styles.header}
         style={{ backgroundColor: `${newNote[0].color}`, color: 'white' }}>
-        {isMobile && (
-          <button  className={styles.btn} onClick={() => handelClick()}>
-            <img src={backBtn} alt="arrow icon" />
-          </button>
-        )}
+        <button className={styles.btn} onClick={() => handelClick()}>
+          <img src={backBtn} alt="arrow icon" />
+        </button>
         <SideBarOptions item={newNote[0]} />
       </div>
-
 
       <div className={styles.content}>
         {notesData.length > 0 ? (
           notesData.map((note, idx) => <Content note={note} key={idx} />)
         ) : (
-          <p className={styles.placeholder}> <br/>  </p>
+          <p className={styles.placeholder}><br/></p>
         )}
       </div>
+
       <div className={styles.Notes}>
         <Notes setNotesData={setNotesData} />
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default NotesByKey
+export default NotesByKey;
